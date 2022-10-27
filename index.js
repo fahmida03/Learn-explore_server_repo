6d0bd972f83e29e961052557bcb1e2a392b6eb30
Fahmida Muntasim <fahmidamuntasimt@gmail.com>
2022-10-27 21:20:24 +0600

    final

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const courses = require('./data/courses.json');
+const allCourses = require('./data/course.json');
+const course = require('./data/course.json')
+
+app.get('/per-courses/:id', (req, res)=> {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.course_id === id);
+    res.send(selectedCourse);
+})
+
+app.get('/per-courses',(req, res)=>{
+    res.send(allCourses)
+})
 app.get('/', (req, res) => {
     res.send('news API Running')
 });
 
+app.get('/all-courses', (req, res) => {
+    res.send(courses)
+})
 
 app.listen(port, () => {
     console.log('Learning server running on port', port);
